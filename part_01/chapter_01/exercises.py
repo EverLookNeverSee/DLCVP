ca07ee65da000b6811ba63b2e01b9e7adbf0d5c5
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2020-10-06 20:51:20 +0330

    doing exercises of chapter_01

---
+++ part_01/chapter_01/exercises.py
@@ -0,0 +1,8 @@
+# importing relevant libraries
+import torch
+
+# returning True if pytorch library is available on our local machine
+torch.cuda.is_available()
+
+# returning the version of pytorch that we've installed
+torch.__version__
