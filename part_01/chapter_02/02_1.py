9fe7bc032ba3e765db2ced3b484f11e2ac0d46ac
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2020-10-11 17:58:36 +0330

    preprocessing

--- part_01/chapter_02/02_1.py
+++ part_01/chapter_02/02_1.py
@@ -11,3 +11,14 @@ resnet = models.resnet101(pretrained=True)
 
 # printing the architecture of resnet neural network
 print(resnet)
+
+# preprocessing
+preprocess = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    )
+])
