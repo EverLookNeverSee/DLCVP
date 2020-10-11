f2c2006fa4b1208d8ed56c68d3287ed3d9ffd390
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2020-10-11 17:45:09 +0330

    creating instances of two modes

--- part_01/chapter_02/02_1.py
+++ part_01/chapter_02/02_1.py
@@ -3,3 +3,7 @@ from torchvision import models
 
 # printing list of all models located in torchvision.models
 print(dir(models))
+
+# creating instances of two modes
+alexnet = models.AlexNet()
+resnet = models.resnet101(pretrained=True)
