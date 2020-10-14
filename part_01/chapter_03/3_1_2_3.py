1f7ece67429a11cb4f092d4cfa0d4f40b9fe2e39
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2020-10-14 20:41:23 +0330

    Adding 3.2.2

--- part_01/chapter_03/3_1_2_3.py
+++ part_01/chapter_03/3_1_2_3.py
@@ -13,3 +13,24 @@ print(f"third element: {a[2]}")
 
 # all elements of list
 print(f"all elements: {a}")
+
+# -------------------------------------------------------------------
+
+# 3.2.2  constructing our first tensors
+
+# importing the torch module
+import torch
+
+# creating a one-dimensional tensor of size 3 filled by 1s
+a = torch.ones(3)
+print(f"a: {a}")
+
+# second element of tensor
+print(f"second element of tensor: {a[1]}")
+
+# converting to float
+print(f"to float: {float(a[1])}")
+
+# assigning a new value to element of tensor
+a[2] = 2.0
+print(f"all elements of tensor: {a}")
