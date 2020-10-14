700a81bffab858288fee87485b43d3a5c90905f6
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2020-10-14 20:32:24 +0330

    Adding 3.2.1

---
+++ part_01/chapter_03/3_1_2_3.py
@@ -0,0 +1,15 @@
+# 3.2.1  from python lists to pytorch tensors
+
+a = [1.0, 2.0, 3.0]
+
+# first element of list
+print(f"first element: {a[0]}")
+
+# second element of list
+print(f"second element: {a[1]}")
+
+# third element of list
+print(f"third element: {a[2]}")
+
+# all elements of list
+print(f"all elements: {a}")
